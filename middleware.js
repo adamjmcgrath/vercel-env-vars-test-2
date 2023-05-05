20bcd853bbe3c8efdac86b41b64c7c39f5992316
Adam Mcgrath <adamjmcgrath@gmail.com>
2023-05-05 14:06:06 +0100

    test branch url

--- middleware.js
+++ middleware.js
@@ -1,11 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 export function middleware(request) {
   return new NextResponse(
-    `MY_ENV_VAR_FROM_FILE=${process.env.MY_ENV_VAR_FROM_FILE}
-PUBLIC_MY_ENV_VAR_FROM_FILE=${process.env.PUBLIC_MY_ENV_VAR_FROM_FILE}
-MY_ENV_VAR_FROM_DASHBOARD=${process.env.MY_ENV_VAR_FROM_DASHBOARD}
-PUBLIC_MY_ENV_VAR_FROM_DASHBOARD=${process.env.PUBLIC_MY_ENV_VAR_FROM_DASHBOARD}
-VERCEL_BRANCH_URL=${process.env.VERCEL_BRANCH_URL}`,
+    `VERCEL_BRANCH_URL=${process.env.VERCEL_BRANCH_URL}
+MY_VERCEL_BRANCH_URL=${process.env.MY_VERCEL_BRANCH_URL}
+PUBLIC_MY_VERCEL_BRANCH_URL=${process.env.PUBLIC_MY_VERCEL_BRANCH_URL}
+`,
     { status: 200, headers: { "content-type": "text/plain" } }
   );
 }
