88511c214482f31ee9515d1139178663e21d8357
Adam Mcgrath <adamjmcgrath@gmail.com>
2023-05-05 14:06:39 +0100

    Add dashboard env vars

--- middleware.js
+++ middleware.js
@@ -4,7 +4,8 @@ export function middleware(request) {
     `VERCEL_BRANCH_URL=${process.env.VERCEL_BRANCH_URL}
 MY_VERCEL_BRANCH_URL=${process.env.MY_VERCEL_BRANCH_URL}
 PUBLIC_MY_VERCEL_BRANCH_URL=${process.env.PUBLIC_MY_VERCEL_BRANCH_URL}
-`,
+MY_VERCEL_BRANCH_URL_DASHBOARD=${process.env.MY_VERCEL_BRANCH_URL_DASHBOARD}
+PUBLIC_MY_VERCEL_BRANCH_URL_DASHBOARD=${process.env.PUBLIC_MY_VERCEL_BRANCH_URL_DASHBOARD}`,
     { status: 200, headers: { "content-type": "text/plain" } }
   );
 }
