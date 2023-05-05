78c2ad8c38484138f91e3719515ab4a459d1fadb
Adam Mcgrath <adamjmcgrath@gmail.com>
2023-05-05 14:08:37 +0100

    Add NEXT_ prefix

--- middleware.js
+++ middleware.js
@@ -3,7 +3,7 @@ export function middleware(request) {
   return new NextResponse(
     `VERCEL_BRANCH_URL=${process.env.VERCEL_BRANCH_URL}
 MY_VERCEL_BRANCH_URL=${process.env.MY_VERCEL_BRANCH_URL}
-PUBLIC_MY_VERCEL_BRANCH_URL=${process.env.PUBLIC_MY_VERCEL_BRANCH_URL}
+NEXT_PUBLIC_MY_VERCEL_BRANCH_URL=${process.env.NEXT_PUBLIC_MY_VERCEL_BRANCH_URL}
 MY_VERCEL_BRANCH_URL_DASHBOARD=${process.env.MY_VERCEL_BRANCH_URL_DASHBOARD}
 PUBLIC_MY_VERCEL_BRANCH_URL_DASHBOARD=${process.env.PUBLIC_MY_VERCEL_BRANCH_URL_DASHBOARD}`,
     { status: 200, headers: { "content-type": "text/plain" } }
